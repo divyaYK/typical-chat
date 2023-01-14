5d30c6bcec2a23ebb137d224fd471f5944b490a5
sidv1905 <sidvarangaonkar1905@gmail.com>
2023-01-14 13:32:24 +0530

    fixed toggle button, refactor needed

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,31 +4,34 @@ import { Icon } from "../icon/Icon";
 import ThemeButton from "../theme/ThemeButton";
 import { Text } from "../typography/Text";
 import {
-  StyledHeader,
-  StyledHeaderActions,
-  StyledHeaderLogo,
+    StyledHeader,
+    StyledHeaderActions,
+    StyledHeaderLogo,
 } from "./HeaderStyles";
 
-const Header = () => {
-  const { isDarkMode } = useDarkMode();
-  return (
-    <StyledHeader>
-      <StyledHeaderLogo>
-        <Icon width="30px" icon="Buildings" />
-        <Text
-          as="h1"
-          variant="h3"
-          mode={isDarkMode ? ThemeEnum.DARK : ThemeEnum.LIGHT}
-        >
-          Space
-        </Text>
-      </StyledHeaderLogo>
-      <StyledHeaderActions>
-        <ThemeButton />
-        <Icon width="30px" icon="Bell" color="white" />
-      </StyledHeaderActions>
-    </StyledHeader>
-  );
+const Header = ({ isDarkMode, enable, disable }: any) => {
+    return (
+        <StyledHeader>
+            <StyledHeaderLogo>
+                <Icon width="30px" icon="Buildings" />
+                <Text
+                    as="h1"
+                    variant="h3"
+                    mode={isDarkMode ? ThemeEnum.DARK : ThemeEnum.LIGHT}
+                >
+                    Space
+                </Text>
+            </StyledHeaderLogo>
+            <StyledHeaderActions>
+                <ThemeButton
+                    isDarkMode={isDarkMode}
+                    enable={enable}
+                    disable={disable}
+                />
+                <Icon width="30px" icon="Bell" color="white" />
+            </StyledHeaderActions>
+        </StyledHeader>
+    );
 };
 
 export default Header;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,27 +4,27 @@ import Sidebar from "../components/sidebar/Sidebar";
 import ChatWindow from "../components/chat/ChatsWindow";
 
 const StyledHomePageWrapper = styled.div`
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
 `;
 
 const StyledHomePageContentWrapper = styled.div`
-  width: 100%;
-  flex-grow: 1;
-  display: flex;
-  flex-direction: row;
+    width: 100%;
+    flex-grow: 1;
+    display: flex;
+    flex-direction: row;
 `;
 
-const HomePage = () => (
-  <StyledHomePageWrapper>
-    <Header />
-    <StyledHomePageContentWrapper>
-      <Sidebar />
-      <ChatWindow />
-    </StyledHomePageContentWrapper>
-  </StyledHomePageWrapper>
+const HomePage = ({ isDarkMode, enable, disable }: any) => (
+    <StyledHomePageWrapper>
+        <Header isDarkMode={isDarkMode} enable={enable} disable={disable} />
+        <StyledHomePageContentWrapper>
+            <Sidebar />
+            <ChatWindow />
+        </StyledHomePageContentWrapper>
+    </StyledHomePageWrapper>
 );
 
 export default HomePage;
